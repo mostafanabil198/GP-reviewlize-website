a31f3b637939275f04a23c8634a6e529ee087e06
mostafa <mostafanabil198@gmail.com>
2021-05-12 05:06:57 +0200

    update scrapper to accept more than 1 request at a time

--- app/services/amazon_product_scrapper.rb
+++ app/services/amazon_product_scrapper.rb
@@ -1,20 +1,20 @@
 class AmazonProductScrapper < Kimurai::Base
-  @@reviews_results = []
+
   def self.scrape(product_url)
-    @@reviews_results.clear
     product_url = product_url.split("dp/").first + "product-reviews/" + product_url.split("dp/").last[0...-1]
     @start_urls = [product_url]
-    self.crawl!
-    return @@reviews_results
+    # self.crawl!
+    return self.parse!(:parse, url: product_url)
   end
   @name = "amazon_product_scrapper"
   # @engine = :selenium_chrome
 
   def parse(response, url:, data: {})
+    @reviews_results ||= []
     response.css(".a-section.review.aok-relative").each do |a|
       next if a.css(".a-section.a-spacing-none.a-spacing-top-small.cr-translate-this-review-section").present?
       review = a.css(".a-size-base.review-text.review-text-content span:not(.aok-hidden)").text.squish
-      @@reviews_results << review if review.present?
+      @reviews_results << review if review.present?
     end
 
     next_page = response.css(".a-pagination li:last-of-type a")
@@ -22,6 +22,6 @@ class AmazonProductScrapper < Kimurai::Base
       puts "----> #{next_page}"
       request_to :parse, url:URI.encode("https://www.amazon.com#{next_page.attribute("href").value.squish}")
     end
+    return @reviews_results
   end
-
 end
--- app/services/amazon_search_scrapper.rb
+++ app/services/amazon_search_scrapper.rb
@@ -1,17 +1,15 @@
 class AmazonSearchScrapper < Kimurai::Base
-  @@search_results = []
   def self.scrape(search_word)
-    @@search_results.clear
-    @@search_word = search_word
-    self.crawl!
-    return @@search_results
+    # self.crawl!
+    return self.parse!(:parse, url: "https://www.amazon.com/?#{search_word}")
   end
   @name = "amazon_search_scrapper"
   # @engine = :selenium_chrome
   @start_urls = ["https://www.amazon.com/"]
 
   def parse(response, url:, data: {})
-    browser.fill_in "field-keywords", with: @@search_word
+    @search_results = []
+    browser.fill_in "field-keywords", with: url.split("?").last
     browser.click_on "Go"
 
     # Update response to current response after interaction with a browser
@@ -30,7 +28,8 @@ class AmazonSearchScrapper < Kimurai::Base
       image = "#{a.css("img").attribute('src').value}"
       price = "#{a.css(".a-price-whole").text.squish}#{a.css(".a-price-fraction").text.squish}#{a.css(".a-price-symbol").text.squish}"
       # puts("=-=-=-=-=-=-=")
-      @@search_results << {title: title, price: price, url: url, image: image}
+      @search_results << {title: title, price: price, url: url, image: image}
     end
+    return @search_results
   end
 end
