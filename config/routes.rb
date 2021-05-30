a82d72eda4468decd02877fa9ad42ae08b474f50
tarek213 <tarek.mohamed2009@yahoo.com>
2021-05-30 02:20:16 +0200

    devise gem done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'reviewlize#home'
   get 'search', to: "reviewlize#index", as: :search
